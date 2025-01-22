084f0ba67496ff3465c8a473e2f021468d8e00b1
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2025-01-22 20:46:24 +0900

    X-Auth-Token ヘッダで認証する処理を追加した。

--- src/redfish_service/authenticate.py
+++ src/redfish_service/authenticate.py
@@ -1,13 +1,40 @@
 from functools import wraps
 from inspect import Parameter, Signature, signature
-from typing import Any, Callable
+from typing import Any, Callable, cast
 
 from fastapi import Depends, Request
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from pydantic import BaseModel
 
 from .exception import ResourceAtUriUnauthorizedError
+from .model.session import Session
+from .repository import instances
 
-basic_auth = HTTPBasic()
+
+class AuthTokenCredentials(BaseModel):
+    token: str
+
+
+class HttpBasicOrAuthToken(HTTPBasic):
+    async def __call__(  # type: ignore
+        self, request: Request
+    ) -> HTTPBasicCredentials | AuthTokenCredentials | None:
+        token = request.headers.get("X-Auth-Token")
+        if token:
+            return AuthTokenCredentials(token=token)
+        else:
+            return await super().__call__(request)
+
+
+basic_or_token_auth = HttpBasicOrAuthToken()
+
+
+def check_token_auth(token: str) -> bool:
+    for session in instances.enum_by_type(Session):
+        if session.extra_fields["token"] == token:
+            return True
+
+    return False
 
 
 def check_basic_auth(username: str, password: str) -> bool:
@@ -18,11 +45,18 @@ def check_basic_auth(username: str, password: str) -> bool:
 def authenticate(f: Callable) -> Callable:
     @wraps(f)
     async def wrapper(*args: list[Any], **kwds: dict[str, Any]) -> Any:
-        req: Request = kwds.get("request")  # type: ignore
-        auth: HTTPBasicCredentials = kwds.pop("auth")  # type: ignore
-        if not check_basic_auth(auth.username, auth.password):
+        req = cast(Request, kwds.get("request"))
+        auth = cast(Any, kwds.pop("auth"))
+
+        if isinstance(auth, HTTPBasicCredentials) and not check_basic_auth(
+            auth.username, auth.password
+        ):
             uri = req.url.path
             raise ResourceAtUriUnauthorizedError(uri, "Invalid username or password")
+
+        if isinstance(auth, AuthTokenCredentials) and not check_token_auth(auth.token):
+            uri = req.url.path
+            raise ResourceAtUriUnauthorizedError(uri, "Invalid X-Auth-Token")
 
         # TODO: session management
         return await f(*args, **kwds)
@@ -30,8 +64,8 @@ def authenticate(f: Callable) -> Callable:
     auth = Parameter(
         "auth",
         Parameter.POSITIONAL_OR_KEYWORD,
-        default=Depends(basic_auth),
-        annotation=HTTPBasicCredentials,
+        default=Depends(basic_or_token_auth),
+        annotation=HTTPBasicCredentials | AuthTokenCredentials,
     )
 
     sig = signature(wrapper)
