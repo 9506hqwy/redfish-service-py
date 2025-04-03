bf3cbf66ef88a9f379695e08b58d4cb8be25dac5
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2025-04-03 19:43:23 +0900

    プロファイラを追加した。

--- src/redfish_service/cli.py
+++ src/redfish_service/cli.py
@@ -3,6 +3,7 @@ import os
 from argparse import ArgumentParser, Namespace
 
 import uvicorn
+from fastapi_profiler import PyInstrumentProfilerMiddleware  # type: ignore
 
 from .repository import init_instances
 from .server import app
@@ -16,12 +17,22 @@ def parse_args() -> Namespace:
     parser.add_argument("-b", "--bind", default=bind)
     parser.add_argument("-p", "--port", default=int(port), type=int)
     parser.add_argument("-d", "--debug", action="store_true")
+    parser.add_argument("--profile", action="store_true")
     return parser.parse_args()
 
 
 def main() -> None:
     args = parse_args()
     log_level = logging.DEBUG if args.debug else logging.INFO
+    if args.profile:
+        app.add_middleware(
+            PyInstrumentProfilerMiddleware,
+            server_app=app,
+            profiler_output_type="html",
+            is_print_each_request=False,
+            html_file_name="profile.html",
+        )
+
     init_instances()
     uvicorn.run(app, host=args.bind, port=args.port, log_level=log_level)
 
